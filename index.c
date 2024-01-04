c760123861e690704c5a1ab1addba400b82fb446
Patel-love <manavamrutiya29@gmail.com>
2024-01-04 12:04:55 +0530

    hii

---
+++ index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+// --------------------------------que-1--------------------------------------
+for(int i=1 ; i<=8; i++){
+    printf("* ");
+}
+printf("\n");
+
+for(int i=1 ; i<=8; i++){
+    printf("* ");
+}
+printf("\n");
+
+for(int i=1 ; i<=8; i++){
+    printf("* ");
+}
+printf("\n");
+for(int i=1 ; i<=8; i++){
+    printf("* ");
+}
+printf("\n");
+
+
+
+
+
+    return 0;
+}
